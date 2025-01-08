99b5b1217bc7e7c59b12b44480b65a80f0260216
TomYaMn <tomyam0206@gmail.com>
2025-01-08 18:40:56 +0800

    initiate react skeleton

---
+++ uranus-explorer-react-app/src/pages/About/About.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./About.module.css";
+
+const About: React.FC = () => {
+  return <h1>Welcome to the About Page</h1>;
+};
+
+export default About;
---
+++ uranus-explorer-react-app/src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.module.css";
+
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
