88923d58ea58c04fe3a7de661b78f2911333dd82
TomYaMn <tomyam0206@gmail.com>
2025-01-10 15:14:28 +0800

    add some aethetic design on home page, tbc

--- uranus-explorer-react-app/src/pages/Home/Home.tsx
+++ uranus-explorer-react-app/src/pages/Home/Home.tsx
@@ -4,7 +4,14 @@ import styles from "./Home.module.css";
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Welcome to the Home Page</h1>
+      <div className={styles.scene}>
+        <div className={styles.galaxy}>
+          <img src="/images/galaxy_spiral_1.jpg" alt="Galaxy Spiral" />
+        </div>
+        <div className={styles.uranus}>
+          <img src="/images/uranus.png" alt="Uranus" />
+        </div>
+      </div>
     </div>
   );
 };
