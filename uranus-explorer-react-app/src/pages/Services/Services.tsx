f515b691b27340d220a679895c8944086a3de13c
TomYaMn <tomyam0206@gmail.com>
2025-01-25 19:28:28 +0800

    add loading component

--- uranus-explorer-react-app/src/pages/Services/Services.tsx
+++ uranus-explorer-react-app/src/pages/Services/Services.tsx
@@ -3,6 +3,7 @@ import styles from "./Services.module.css";
 import Button from "../../components/Button/Button";
 import { fetchWithAuth } from "../../utils/tokenUtils";
 import { Service } from "../../interfaces/Services";
+import Loading from "../../components/Loading/Loading";  // Import the Loading component
 
 const Services: React.FC = () => {
   const [services, setServices] = useState<Service[]>([]);
@@ -32,33 +33,31 @@ const Services: React.FC = () => {
 
   return (
     <div className={styles.Services}>
-      {services.length === 0 ? (
-        <div className={styles.loading}>Loading...</div>
-      ) : (
-        services.length === 0 && (
-          <div className={styles.noServices}>No services available</div>
-        )
-      )}
+      {loading && <Loading />} {/* Show loading spinner while fetching data */}
 
-      {services.map((service, index) => (
-        <div key={index} className={styles.card}>
-          <div className={styles.serviceContent}>
-            <div className={styles.columnOne}>
-              <h2 className={styles.categoryTitle}>{service.category}</h2>
-              <Button label="Apply" />
-            </div>
-            <div className={styles.columnTwo}>
-              <ul className={styles.serviceList}>
-                {service.serviceItems.map((item, itemIndex) => (
-                  <li key={itemIndex} className={styles.serviceItem}>
-                    {item.item}
-                  </li>
-                ))}
-              </ul>
+      {services.length === 0 ? (
+        <div className={styles.noServices}>No services available</div>
+      ) : (
+        services.map((service, index) => (
+          <div key={index} className={styles.card}>
+            <div className={styles.serviceContent}>
+              <div className={styles.columnOne}>
+                <h2 className={styles.categoryTitle}>{service.category}</h2>
+                <Button label="Apply" />
+              </div>
+              <div className={styles.columnTwo}>
+                <ul className={styles.serviceList}>
+                  {service.serviceItems.map((item, itemIndex) => (
+                    <li key={itemIndex} className={styles.serviceItem}>
+                      {item.item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
